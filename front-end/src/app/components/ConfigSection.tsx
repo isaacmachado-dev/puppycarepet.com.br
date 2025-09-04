f957763505f0b84a3e7e46d6eb76b987ab5567df
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-09-04 13:09:57 -0300

    feat: our services and footer early sections

--- front-end/src/app/components/ConfigSection.tsx
+++ front-end/src/app/components/ConfigSection.tsx
@@ -7,8 +7,7 @@ interface SectionProps {
 
 export function SectionYellow({ children, className = "" }: SectionProps) {
   return (
-    <section className={`bg-[#FECE14] py-30 sm:py-30 p-5 sm:p-10 flex flex-col justify-center 
-    ${className}`}>
+    <section className={`bg-[#FECE14] py-25 sm:py-30 p-5 sm:p-10 flex flex-col justify-center text-black ${className}`}>
       {children}
     </section>
   );
@@ -16,8 +15,7 @@ export function SectionYellow({ children, className = "" }: SectionProps) {
 
 export function SectionPurple({ children, className = "" }: SectionProps) {
   return (
-    <section className={`bg-[#1A112E] py-30 sm:py-30  p-5 sm:p-10 flex flex-col justify-center 
-    ${className}`}>
+    <section className={`bg-[#1A112E] py-25 sm:py-30 p-5 sm:p-10 flex flex-col justify-center items-center ${className}`}>
       {children}
     </section>
   );
@@ -26,7 +24,7 @@ export function SectionPurple({ children, className = "" }: SectionProps) {
 export function SetupSection({ children, className = "" }: SectionProps) {
 
   return (
-    <div className={`w-full max-w-[1000px] mx-auto px-5 sm:px-10 text-white ${className}`}>
+    <div className={`w-full max-w-[1200px] mx-auto px-5 sm:px-10 ${className}`}>
       {children}
     </div>
   );
--- front-end/src/app/sections/OurServices.tsx
+++ front-end/src/app/sections/OurServices.tsx
@@ -1,13 +1,49 @@
-import { SectionYellow } from "../components/ConfigSection";
+import { SectionYellow, SetupSection } from "../components/ConfigSection";
 import H2 from "../components/TextSection";
 
 
 export default function OurServices() {
     return (
         <SectionYellow>
-            <H2>
-                Conheça os nossos serviços
-            </H2>
-        </SectionYellow>
+            <SetupSection>
+
+                <H2>
+                    Conheça os nossos serviços
+                </H2>
+
+                <div className="flex flex-col sm:flex-row gap-5 sm:gap-20 justify-center items-center mt-5">
+                    <p className="max-w-[400px] text-4xl text-left justify-center">
+                        Atendemos o seu animalzinho junto a
+                        uma veterinária qualificada, garantindo
+                        um trabalho com responsabilidade de quem entende.
+                    </p>
+
+                    <div className="grid grid-cols-2 gap-5 mt-5">
+
+                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
+                            Leva e traz
+                        </p>
+
+                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
+                            Banho & Tosa
+                        </p>
+
+                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
+                            Cães e Gatos
+                        </p>
+
+                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
+                            Roupinhas & Acessórios
+                        </p>
+                    </div>
+
+                </div>
+            </SetupSection>
+
+            <div>
+
+            </div>
+
+        </SectionYellow >
     );
 }
