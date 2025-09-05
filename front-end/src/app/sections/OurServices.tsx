dcd62a97b4713eb77a1f1effd84a4b5da3a47f27
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-09-05 00:59:29 -0300

    fix: logo and icons adding

--- front-end/src/app/sections/OurServices.tsx
+++ front-end/src/app/sections/OurServices.tsx
@@ -1,6 +1,7 @@
 import { SectionYellow, SetupSection } from "../components/ConfigSection";
 import H2 from "../components/TextSection";
 
+import { CarFront, ShowerHead, Dog, ShoppingCart } from 'lucide-react';
 
 export default function OurServices() {
     return (
@@ -11,30 +12,33 @@ export default function OurServices() {
                     Conheça os nossos serviços
                 </H2>
 
-                <div className="flex flex-col sm:flex-row gap-5 sm:gap-20 justify-center items-center mt-5">
-                    <p className="max-w-[400px] text-4xl text-left justify-center">
+                <div className="flex flex-col sm:flex-row gap-5 justify-center items-start mt-5">
+                    <p className="max-w-[400px] text-4xl text-left">
                         Atendemos o seu animalzinho junto a
                         uma veterinária qualificada, garantindo
                         um trabalho com responsabilidade de quem entende.
                     </p>
 
-                    <div className="grid grid-cols-2 gap-5 mt-5">
+                    <div className="grid grid-cols-2 gap-5 mt-5 sm:mt-0">
+                        <div className="px-5 py-2.5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex gap-3 items-center justify-center h-[150px]">
+                            <CarFront size={40} className="text-black" />
+                            <p>Leva e Traz</p>
+                        </div>
 
-                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
-                            Leva e traz
-                        </p>
+                        <div className="px-5 py-2.5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex gap-3 items-center justify-center h-[150px]">
+                            <ShowerHead size={40} className="text-black" />
+                            <p>Banho e Tosa</p>
+                        </div>
 
-                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
-                            Banho & Tosa
-                        </p>
+                        <div className="px-5 py-2.5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex gap-3 items-center justify-center h-[150px]">
+                            <Dog size={40} className="text-black" />
+                            <p>Cães e Gatos</p>
+                        </div>
 
-                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
-                            Cães e Gatos
-                        </p>
-
-                        <p className="px-15 py-5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex items-center justify-center text-center min-h-[150px]">
-                            Roupinhas & Acessórios
-                        </p>
+                        <div className="px-5 py-2.5 bg-[#B3B3B3] rounded-lg text-xl sm:text-2xl text-black font-normal flex gap-3 items-center justify-center h-[150px]">
+                            <ShoppingCart size={40} className="text-black" />
+                            <p>Roupinhas</p>
+                        </div>
                     </div>
 
                 </div>
