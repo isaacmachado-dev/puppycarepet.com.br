97105ebfffd4026d818fbed19350f6c1553fc000
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-09-05 20:16:08 -0300

    feat: services section upgrade

--- front-end/src/app/components/Footer.tsx
+++ front-end/src/app/components/Footer.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className="flex p-20 justify-center bg-[#171717]">
-            <Image src="/Footer/footer-puppycare.png" alt="Footer Logo"
+            <Image src="/Footer/footer-puppycare.png" alt="Footer logo letrada Puppy Care"
                 width={300}
                 height={300}
-
 
             />
         </footer>
