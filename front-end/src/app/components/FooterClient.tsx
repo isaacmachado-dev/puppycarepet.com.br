ed197ac561efdf9f5282c0ac23416cee272ea202
calixtoanacarla <accalixtoanjos@gmail.com>
2025-09-16 23:13:55 -0300

    fix

--- front-end/src/app/components/FooterClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { usePathname } from "next/navigation";
-import Footer from "./Footer";
-
-export default function FooterClient() {
-    const pathname = usePathname();
-    if (pathname === "/login") return null;
-    return <Footer />;
-}
