03f1737d43e52490fb61116a5a19e339582171a2
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-09-17 22:12:02 -0300

    fix: merging

---
+++ front-end/src/app/components/FooterClient.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
+
+export default function FooterClient() {
+    const pathname = usePathname();
+    if (pathname === "/login" || pathname === "/admin") return null;
+    return <Footer />;
+
+}
