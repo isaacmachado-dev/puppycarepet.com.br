431099b6862e8cc63edcd146c9e76c340f4bc1bd
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-11-14 14:41:38 -0300

    Fix: Removing unecessary JS and Map files

--- back-end/dist/src/clientes/dto/create-cliente.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CreateClienteDto {
-    NOME: string;
-    TELEFONE: string;
-    ENDERECO: string;
-}
--- back-end/dist/src/pacotes/dto/update-pacote.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreatePacoteDto } from './create-pacote.dto';
-declare const UpdatePacoteDto_base: import("@nestjs/common").Type<Partial<CreatePacoteDto>>;
-export declare class UpdatePacoteDto extends UpdatePacoteDto_base {
-}
-export {};
