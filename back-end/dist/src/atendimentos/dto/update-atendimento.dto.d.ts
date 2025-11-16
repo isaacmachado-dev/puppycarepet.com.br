755b7a6b2641585c9b1e5ee98b709698a5fd12c2
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-11-16 11:07:00 -0300

    feat: admin page Agendamentos responsivity

--- back-end/dist/src/atendimentos/dto/update-atendimento.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateAtendimentoDto } from './create-atendimento.dto';
-declare const UpdateAtendimentoDto_base: import("@nestjs/common").Type<Partial<CreateAtendimentoDto>>;
-export declare class UpdateAtendimentoDto extends UpdateAtendimentoDto_base {
-}
-export {};
--- front-end/src/app/admin/agendamento/components/sections/Atendendo.tsx
+++ front-end/src/app/admin/agendamento/components/sections/Atendendo.tsx
@@ -1,18 +1,32 @@
 import { Dog } from "lucide-react";
+import Image from "next/image";
 
 export function Atendendo() {
     return (
         <div>
-            <div className="flex gap-2 items-center p-2 border-2 border-black rounded-md">
+            <div className="p-2 border-2 border-black rounded-md w-max mx-auto">
 
-                <div className="flex gap-2 items-center">
-                    <Dog />
-                    Malu
+                <div className="flex flex-col  items-center justify-center ">
+
+                    <img
+                        src="https://placehold.co/300x200?text=Imagem"
+                        alt="Dog"
+                        width={300}
+                        height={300}
+                    />
+
+                    <div className="flex flex-row gap-2">
+                        <Dog
+                            className="w-8 h-8"
+                        />
+                        <span className="font-bold text-lg">
+                            Malu
+                        </span>
+                    </div>
+                
+
                 </div>
 
-                <div className="m-2">
-                    <span className="text-gray-500"> - Banho e Tosa </span>
-                </div>
 
 
             </div>
