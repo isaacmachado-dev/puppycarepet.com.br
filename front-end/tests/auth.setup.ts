05ad2398e0b1d07eea5b574273e3ba4ccfcb29da
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-12-14 11:44:16 -0300

    feat: playwright test more tests

--- front-end/tests/auth.setup.ts
+++ front-end/tests/auth.setup.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import * as fs from 'fs';
 
 const authFile = path.join(__dirname, '.auth/user.json');
+const EMAIL = process.env.PLAYWRIGHT_TEST_EMAIL || '';
+const PASSWORD = process.env.PLAYWRIGHT_TEST_PASSWORD || ''
 
 setup('Setup de login com estado salvo', async ({ page }) => {
 
@@ -10,9 +12,9 @@ setup('Setup de login com estado salvo', async ({ page }) => {
     await page.getByRole('link', { name: 'Agende agora' }).click();
     await page.getByRole('link', { name: 'Acesse sua conta' }).click();
     await page.getByRole('textbox', { name: 'E-mail' }).click();
-    await page.getByRole('textbox', { name: 'E-mail' }).fill('joel.miller@puppycarepet.com.br');
+    await page.getByRole('textbox', { name: 'E-mail' }).fill(EMAIL);
     await page.getByRole('textbox', { name: 'E-mail' }).press('Tab');
-    await page.getByRole('textbox', { name: 'Senha' }).fill('admin123');
+    await page.getByRole('textbox', { name: 'Senha' }).fill(PASSWORD);
     await page.getByRole('button', { name: 'Entrar' }).click();
     // 1) Ver para onde você foi
 
