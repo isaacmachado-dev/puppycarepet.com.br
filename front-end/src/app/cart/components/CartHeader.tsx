fdbb2151cbfa8d1acf3c13583de8acef6ce46aae
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-12-15 21:16:12 -0300

    Feat: Including HREF on header logo and fix 'Porte' dropdown

--- front-end/src/app/cart/components/CartHeader.tsx
+++ front-end/src/app/cart/components/CartHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartHeaderProps {
   label: string;
@@ -8,25 +9,28 @@ interface CartHeaderProps {
   priority?: boolean;
 }
 
-export const CartHeader: React.FC<CartHeaderProps> = ({ label, text, className = "object-contain", priority = true }) => {
+export const CartHeader: React.FC<CartHeaderProps> = ({
+  label,
+  text,
+  className = "object-contain",
+  priority = true,
+}) => {
   return (
     <div className="flex flex-row justify-start mb-6 gap-[3px] items-center">
-      <Image
-        src="/logos/brand/logo-redondo-rosa.png"
-        alt="Petshop Puppy Care"
-        width={50}
-        height={50}
-        className={className}
-        priority={priority}
-      />
+      <Link href="/">
+        <Image
+          src="/logos/brand/logo-redondo-rosa.png"
+          alt="Petshop Puppy Care"
+          width={50}
+          height={50}
+          className={className}
+          priority={priority}
+        />
+      </Link>
       <span className="bg-[#FECE14] p-2 m-2 rounded-md text-black font-bold">
         <label>{label}</label>
       </span>
-      {text && (
-        <span className="text-white">
-          {text}
-        </span>
-      )}
+      {text && <span className="text-white">{text}</span>}
     </div>
   );
 };
