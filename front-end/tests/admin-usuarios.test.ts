0b2a2828126f07f99997a606316ca1a01561786d
Isaac Machado <isaacmachado.profissional@gmail.com>
2026-01-27 06:53:16 -0300

    fix: more tests on playwright now working fix: background on admin page fix on mode dark

--- front-end/tests/admin-usuarios.test.ts
+++ front-end/tests/admin-usuarios.test.ts
@@ -9,20 +9,19 @@ test.use({ storageState: STORAGE_STATE_PATH }); // Puxar do save
 test('4 - Testando página de Usuários...', async ({ page }) => {
     await page.goto('http://localhost:3000/admin');
     await page.locator('div').filter({ hasText: 'Usuários' }).nth(5).click();
+    await expect(page.getByText('Carregando...')).toBeHidden({ timeout: 10000 });
 
-    await expect(page.locator('div:has(heading[name="Funcionários"]) ~ generic:nth(0)')).toBeVisible();
-
+    await page.getByRole('img', { name: 'Rock Lee' }).click();
+    await page.locator('#radix-_r_c_').click();
     await page.getByRole('menuitem', { name: 'Editar' }).click();
-
-    await page.getByRole('button', { name: 'Open menu' }).click();
-
-    await page.getByRole('menuitem', { name: 'Editar' }).click();
+    await page.getByRole('textbox', { name: 'Nome' }).fill('Rock Leee');
     await page.getByRole('textbox', { name: 'Email' }).click();
-
-    await page.getByRole('button', { name: 'Administradores' }).click();
-    await page.getByRole('menuitemcheckbox', { name: 'Condutores' }).click();
-    await page.getByRole('button', { name: 'Administradores' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).press('ControlOrMeta+a');
+    await page.getByRole('textbox', { name: 'Email' }).fill('Rock.lee@puppycarepet.com.br');
+    await page.getByRole('button', { name: 'Condutores' }).click();
+    await page.getByRole('menuitemcheckbox', { name: 'Colaborador' }).click();
     await page.getByRole('button', { name: 'Salvar' }).click();
+    console.log('4.1 - Edição de usuário testada com sucesso!');
 
     await page.getByRole('button').nth(3).click();
     await page.getByRole('textbox', { name: 'Nome *' }).fill('teste');
